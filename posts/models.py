0faf9994232e5c4aea0b82500269d9dfced66294
rkpust <rezaul.cse.pust17@gmail.com>
2023-10-02 15:19:37 +0600

    tested Post model exists and created Post Model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+
+class PostModelTest(TestCase):
+    def test_post_model_exists(self):
+        posts = Post.objects.count()
+
+        self.assertEqual(posts,0)
