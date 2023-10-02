93bdfbef61ee170eca8514a735138f54b380954c
rkpust <rezaul.cse.pust17@gmail.com>
2023-10-02 17:05:59 +0600

    tested string representation of objects of Post model

--- posts/tests.py
+++ posts/tests.py
@@ -8,3 +8,11 @@ class PostModelTest(TestCase):
         posts = Post.objects.count()
 
         self.assertEqual(posts,0)
+
+    def test_string_representation_of_objects(self):
+        post = Post.objects.create(
+            title = "Test Post Title",
+            body = "Test Post Body"
+        )
+
+        self.assertEqual(str(post),post.title)
